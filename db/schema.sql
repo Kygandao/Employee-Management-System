bd46994b777e901524ecffe794ed298991f39c65
Kygan <kygandao23@gmail.com>
2021-08-01 19:48:10 -0700

    initial file structure setup & installing dependencies

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+DROP TABLE IF EXISTS department;
+CREATE TABLE department (
+
+);
+
+DROP TABLE IF EXISTS role;
+CREATE TABLE role (
+
+);
+
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee (
+
+);
